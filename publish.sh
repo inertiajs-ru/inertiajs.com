4ee530294688a54c23ae7bbcfa410eff5fc4b4b5
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2020-10-29 14:57:24 +0300

    Мелкие исправления

--- publish.sh
+++ publish.sh
@@ -3,13 +3,6 @@ docs="_gh_pages"
 ghPagesBranch="gh-pages"
 site="inertiajs.ru"
 
-!/bin/sh
-if [ "`git status -s`" ]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
-
 echo "Deleting old publication"
 rm -rf $out
 rm -rf $docs
